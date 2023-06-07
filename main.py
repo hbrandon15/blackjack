09a829b2ac4330744bbbdd060e1b1203fb1f3139
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-06-07 11:12:24 -0500

    Create a deal function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+import art
+import os
+import random
 ############### Our Blackjack House Rules #####################
 
 ## The deck is unlimited in size. 
@@ -11,14 +13,39 @@
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+dealer_hand = []
+player_hand = []
+dealer_score = 0
+player_score = 0
+
 def game_start():
-    user_ready = input("Would you like to play a game of Blackjack? Type 'y' for yes or 'n' for no. ").lower()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(art.logo)
+    user_ready = input("Would you like to play a game of Blackjack? Type 'y' for yes or 'n' for no. \n").lower()
     if(user_ready == 'y'):
-        print("Game starting")
+        print("Game starting...")
+        deal_start()
     elif(user_ready == 'n'):
         print("Maybe next time!")
     else:
         print("Invalid input!")
         game_start()
 
+def deal_start():
+    for range in(0,2):
+      dealer_hand.append(random.choice(cards))
+      player_hand.append(random.choice(cards))
+    player_score = sum(player_hand)
+    
+    print(f"Dealer's first card is: {dealer_hand[0]}")
+    print(f"Your hand is: {player_hand}, with a current score of {player_score}")
+    
+    
 game_start()
+
+
+
+
+# TODO: #2 Create a hit function that will select another card and add it to the total score
+
